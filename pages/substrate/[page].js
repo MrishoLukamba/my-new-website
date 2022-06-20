a04bb25829093ef5b59d5a3fb77b28b619283d00
Mrisho Lukamba <69342343+MrishoLukamba@users.noreply.github.com>
2022-06-20 23:37:13 +0300

    worked5

--- pages/substrate/[page].js
+++ pages/substrate/[page].js
@@ -2,18 +2,20 @@ import React from 'react'
 import {useState, useEffect,useContext} from 'react'
 import AppContext from '../../Component/AppContext'
 import styles from '../../styles/article.module.css'
-import Image from 'next/image'
+
 
 function subpage() {
   const [page, setPage] = useState()
   const value =useContext(AppContext)
   
-console.log(value.state?.article[7]?.paragraph.rich_text[0].annotations.bold ==true)
+console.log(value.state?.article[7]?.paragraph.rich_text[0].annotations)
   return (
     <div className={styles.DivPage}>
      
      {
         value.state.article?.map(data =>{
+          if(data.paragraph?.rich_text[0]?.annotations.italic)
+          return (<span>{data.paragraph.rich_text[0]?.plain_text}</span>)
 
           if(data.paragraph?.rich_text[0]?.annotations.bold)
           return (<h3 >{data.paragraph?.rich_text[0].plain_text}</h3>)
